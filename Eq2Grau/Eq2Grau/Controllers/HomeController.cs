ceb411ac4440aa46f74ad8bc0511aca9147ab58c
joaopfbatista321 <joaopfbatista@gmail.com>
2023-03-23 21:20:33 +0000

    formula raiz quadrada

--- Eq2Grau/Eq2Grau/Controllers/HomeController.cs
+++ Eq2Grau/Eq2Grau/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using Eq2Grau.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Diagnostics.Eventing.Reader;
 
 namespace Eq2Grau.Controllers
 {
@@ -13,10 +14,40 @@ namespace Eq2Grau.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
-        {
+        public IActionResult Index(int A, int B, int C) {
+            //var auxiliar
+            double delta = B * B - 4 * A * C;
+            string x1 = "", x2 = "";
+            //posso calcular as raizes?
+            if (A != 0) {
+                //há raizes reais?
+                if (delta >= 0) {
+                    //sim, há
+                    //x1 = (-b + sqrt(b2-4ac))/(2a)
+                    //x2 = (-b - sqrt(b2-4ac))/(2a)
+                    x1 = (-B + Math.Sqrt(delta)) / (2 * A) + "";
+                    x2 = (-B - Math.Sqrt(delta)) / 2 / A + "";
+                }
+
+                else {
+                    //não há numeros reais
+
+                    x1 = -B / 2 / A + "+" + Math.Sqrt(delta) / 2 / A + "i";
+                    x2 = -B / 2 / A + "-" + Math.Sqrt(delta) / 2 / A + "i";
+                }
+                
+            }
+            
+            //preparar os dados para serem enviados para o servidor 
+            ViewBag.X1 = x1;
+            ViewBag.X2 = x2;
+
             return View();
         }
+
+
+
+
 
         public IActionResult Privacy()
         {
